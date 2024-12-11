70dde99d330b5ec8688353bf3af8c76fd61b65db
Robson Cesar de Siqueira <sistemas.robson@gmail.com>
2024-12-11 14:49:09 -0300

    saveRecipe

--- TestingProject/View/FormRecipeView.swift
+++ TestingProject/View/FormRecipeView.swift
@@ -10,6 +10,7 @@ import SwiftData
 
 struct FormRecipeView: View {
     
+    @Environment(\.dismiss) private var dismiss
     @Environment(\.modelContext) private var modelContext
     @Query(sort: \RecipeIngredient.name, order: .forward) private var ingredients: [RecipeIngredient]
     @State private var name: String = ""
@@ -17,43 +18,66 @@ struct FormRecipeView: View {
     @State private var showAddIngredientsSheet: Bool = false
     
     var body: some View {
-        Form {
-            Section(header: Text("Recipe Name")) {
-                TextField("Enter Recipe Name", text: $name)
-                    .textFieldStyle(.roundedBorder)
-                    .padding()
-            }
-            
-            Section(header: Text("Ingredients")) {
-                if selectedIngredients.isEmpty {
-                    Text("No Ingredients selected.")
-                        .foregroundColor(.gray)
-                } else {
-                    ForEach(selectedIngredients) { ingredient in
-                        HStack {
-                            Text(ingredient.name)
-                            Spacer()
-                            Button(action: {
-                                removeIngredient(ingredient)
-                            }) {
-                                Image(systemName: "minus.circle")
-                                    .foregroundColor(.red)
+        NavigationView {
+            Form {
+                Section(header: Text("Recipe Name")) {
+                    TextField("Enter Recipe Name", text: $name)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                }
+                
+                Section(header: Text("Ingredients")) {
+                    if selectedIngredients.isEmpty {
+                        Text("No Ingredients selected.")
+                            .foregroundColor(.gray)
+                    } else {
+                        ForEach(selectedIngredients) { ingredient in
+                            HStack {
+                                Text(ingredient.name)
+                                Spacer()
+                                Button(action: {
+                                    removeIngredient(ingredient)
+                                }) {
+                                    Image(systemName: "minus.circle")
+                                        .foregroundColor(.red)
+                                }
                             }
                         }
                     }
+                    
+                    Button(action: {
+                        showAddIngredientsSheet.toggle()
+                    }) {
+                        Label("Add Ingredient", systemImage: "plus.circle")
+                    }
                 }
-                
-                Button(action: {
-                    showAddIngredientsSheet.toggle()
-                }) {
-                    Label("Add Ingredient", systemImage: "plus.circle")
+            }
+            .sheet(isPresented: $showAddIngredientsSheet) {
+                IngredientsSelectionView(ingredients: ingredients, selectedIngredients: $selectedIngredients)
+            }
+            .navigationTitle("New Recipe")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
                 }
-                .navigationTitle("New Recipe")
-                .sheet(isPresented: $showAddIngredientsSheet) {
-                    IngredientsSelectionView(ingredients: ingredients, selectedIngredients: $selectedIngredients)
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save") {
+                        saveRecipe()
+                    }
+                    .disabled(name.isEmpty || selectedIngredients.isEmpty)
                 }
             }
         }
+    }
+    
+    private func saveRecipe() {
+        let recipe = FoodRecipe(name: name, ingredients: selectedIngredients)
+        
+        modelContext.insert(recipe)
+        
+        dismiss()
     }
     
     private func removeIngredient(_ ingredient: RecipeIngredient) {
